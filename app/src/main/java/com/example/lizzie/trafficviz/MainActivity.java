62d8296d362fa298005cdef3270f58445c988725
Elizabeth "Lizzie" Siegle <lizzie.siegle@gmail.com>
2016-07-14 17:29:19 -0700

    curr working on location services, uber api

--- app/src/main/java/com/example/lizzie/trafficviz/MainActivity.java
+++ app/src/main/java/com/example/lizzie/trafficviz/MainActivity.java
@@ -1,19 +1,33 @@
 package com.example.lizzie.trafficviz;
 
 import android.app.Activity;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.OvershootInterpolator;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
+import android.net.Uri;
+import android.location.Location;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
 import com.pubnub.api.*;
 import org.json.*;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements ConnectionCallbacks, OnConnectionFailedListener {
 
     Pubnub pn = new Pubnub("demo", "demo");
+    GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    TextView tvLatLong;
 
-    WebView webView;
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,16 +39,64 @@ public class MainActivity extends Activity {
         WebSettings webSetting = webView.getSettings();
         webSetting.setBuiltInZoomControls(true);
         webSetting.setJavaScriptEnabled(true);
+        webSetting.setLoadsImagesAutomatically(true);
         webSetting.setDomStorageEnabled(true); //stackoverflow
+        webSetting.setSupportZoom(true);
+        webSetting.setBuiltInZoomControls(true);
+        webSetting.setDisplayZoomControls(false);
+
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
         //webView.addJavascriptInterface(new WebAppInterface(this));
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("file:///android_asset/eon.html");
+        webView.setWebViewClient(new lizzieBrowser());
+        //webView.setWebChromeClient(new WebChromeClient());
+        String idkMan = Uri.parse("file:///android_asset/eon.html").toString();
+        webView.loadUrl(idkMan);
+
+        tvLatLong = (TextView) findViewById(R.id.tvLatLong);
+
+        buildGoogleApiClient();
+
+        if(mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+        else {
+            Toast.makeText(this, "not connected...", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
-    private class WebViewClient extends android.webkit.WebViewClient {
+    @Override
+    public void onConnectionFailed(ConnectionResult arg0) {
+        Toast.makeText(this, "Failed to connect", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnected(Bundle arg0) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(mLastLocation != null) {
+            tvLatLong.setText("Latitude: "+ String.valueOf(mLastLocation.getLatitude()) + "Longitude: " + String.valueOf(mLastLocation.getLongitude()));
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int arg0) {
+        Toast.makeText(this, "Connection suspended", Toast.LENGTH_SHORT).show();
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    private class lizzieBrowser extends WebViewClient {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
             return super.shouldOverrideUrlLoading(view, url);
+            //return true;
         }
     }
 }
